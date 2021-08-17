c10112bdea2a924912b5791c6acbcbc6afc59fff
mayuri-dhote <mayuri.dhote@knoldus.com>
2021-08-17 09:38:21 +0530

    first commit

---
+++ persistence/src/HelloWorld2.scala
@@ -0,0 +1,5 @@
+object HelloWorld2{
+  def main(args: Array[String]): Unit = {
+    println("Hello World of Persistence");
+  }
+}
---
+++ root/src/HelloWorld3.scala
@@ -0,0 +1,5 @@
+object HelloWorld3{
+  def main(args: Array[String]): Unit = {
+    println("Hello World of Root");
+  }
+}
